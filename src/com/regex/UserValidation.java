f861c702ac759d14447189d7137383c6761ecb66
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-16 18:14:55 +0530

    Added: code to check if password has minimum 8 characters

--- src/com/regex/UserValidation.java
+++ src/com/regex/UserValidation.java
@@ -9,6 +9,21 @@ import java.util.regex.Pattern;
 public class UserValidation {
 
 	/*
+	 * method to validate first name of the user First name starts with caps and has
+	 * minimum 3 characters
+	 * 
+	 * @param firstName of the user
+	 * 
+	 * @return true if first name is valid i.e first name should begin with capital
+	 * with minimum 3 characters if not return false
+	 */
+	public Boolean validateFirstName(String firstName) {
+		String matcher = "^[A-Z]{1}[a-z]{2,}$";
+		Boolean check = Pattern.matches(matcher, firstName);
+		return check;
+	}
+
+	/*
 	 * method to validate last name of the user Last name starts with caps and has
 	 * minimum 3 characters
 	 * 
@@ -19,20 +34,6 @@ public class UserValidation {
 	public Boolean validateLastname(String lastName) {
 		String lastNameMatcher = "^[A-Z]{1}[a-z]{2,}$";
 		Boolean check = Pattern.matches(lastNameMatcher, lastName);
-		return check;
-	}
-
-	/*
-	 * method to validate first name of the user First name starts with caps and has
-	 * minimum 3 characters
-	 * 
-	 * @param firstName of the user
-	 * 
-	 * @return true if first name is valid else returns false
-	 */
-	public Boolean validateFirstName(String firstName) {
-		String matcher = "^[A-Z]{1}[a-z]{2,}$";
-		Boolean check = Pattern.matches(matcher, firstName);
 		return check;
 	}
 
@@ -54,11 +55,25 @@ public class UserValidation {
 	 * 
 	 * @param phone number given as input by the user
 	 * 
-	 * @return true if phone is valid else returns false
+	 * @return true if phone is valid i.e phone number should begin with country
+	 * code followed by 10 digits else returns false
 	 */
 	public Boolean validatePhone(String phone) {
 		String mobileNoMatcher = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
 		Boolean check = Pattern.matches(mobileNoMatcher, phone);
+		return check;
+	}
+
+	/*
+	 * method to validate password entered by the user
+	 * 
+	 * @param password given as input by the user
+	 * 
+	 * @return true if password has minimum 8 characters else returns false
+	 */
+	public Boolean validatePassword(String password) {
+		String passwordMatcher = "^.{8,}";
+		Boolean check = Pattern.matches(passwordMatcher, password);
 		return check;
 	}
 }
